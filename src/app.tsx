6859ca7a35b3ab554fb0a1f21766b885b6c04df2
Xheldon <1006044256@qq.com>
2021-12-07 01:18:06 +0800

    主流程跑通

--- src/app.tsx
+++ src/app.tsx
@@ -10,13 +10,20 @@ import {
   Form,
   Select,
   notification,
+  message,
 } from 'antd';
 
+import { Octokit } from "octokit";
+
 interface Config {
-  github_token: string;
-  name: string;
   config: string;
-  url: string;
+  name: string;
+  github_token: string;
+  github_owner: string;
+  github_repo: string;
+  github_branch: string;
+  github_path: string;
+  github_message: string;
 };
 
 const { Option } = Select;
@@ -26,14 +33,18 @@ const layout = {
   wrapperCol: { span: 16 },
 };
 const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
+  wrapperCol: { span: 24 },
 };
 
 const DEFAULT_CONFIG = {
   config: '',
   name: '',
   github_token: '',
-  url: '',
+  github_owner: '',
+  github_repo: '',
+  github_branch: 'main',
+  github_path: '',
+  github_message: '',
 };
 
 const App: React.FC<{}> = () => {
@@ -58,7 +69,7 @@ const App: React.FC<{}> = () => {
       // Note: 根据应用主题模式，适配 UI，各种颜色配置详见：https://ant-design.gitee.io/docs/react/customize-theme-variable-cn
       ConfigProvider.config({
         theme: {
-            primaryColor: '#eee',
+            primaryColor: '#202020',
         }
       });
     } else {
@@ -167,6 +178,26 @@ const App: React.FC<{}> = () => {
           tableSupported: true,
         })
         console.log('markdown:', markdown);
+        // Note: 此处获取到 markdown，加上所有配置也齐全了，可以开始同步了
+        // TODO: 需要先发送获取该文件的请求，以检查该文件是否存在，如果存在，则需要提供该文件的 sha（在返回的结果中有该值）
+        //  如果不存在则不需要该值
+        const octokit = new Octokit({auth: form.getFieldValue('github_token')});
+        octokit.rest.repos.createOrUpdateFileContents({
+          owner: form.getFieldValue('github_owner'),
+          repo: form.getFieldValue('github_repo'),
+          branch: form.getFieldValue('github_branch') || 'main',
+          path: form.getFieldValue('github_path'),
+          message: form.getFieldValue('github_message'),
+          content: btoa(unescape(encodeURIComponent(markdown))),
+      }).then((data) => {
+          console.log('data:', data);
+          if ([200, 201].includes(data.status)) {
+            message.info('更新成功！');
+          }
+      }).catch((err) => {
+        message.error('更新失败，请打开控制台查看（Web 可以看 Log，Mac 还不行）')
+          console.log('错误:', err);
+      });
       }
     })();
   }, [configList]);
@@ -204,7 +235,7 @@ const App: React.FC<{}> = () => {
               type="info"
               action={
                 <Button size="small" danger onClick={() => {
-                  craft.editorApi.openURL('http://github.com/')
+                  craft.editorApi.openURL('https://jekyllrb.com/docs/front-matter/')
                 }}>
                   详情
                 </Button>
@@ -235,18 +266,36 @@ const App: React.FC<{}> = () => {
                   <Form.Item name="name" label="配置名" rules={[{ required: true }]}>
                     <Input placeholder={'配置项的名字？'} />
                   </Form.Item>
-                  <Form.Item name="github_token" label="Github Token" rules={[{ required: true }]}>
-                    <Input placeholder={'Github Personal Token'} />
+                  <Form.Item
+                    name="github_token"
+                    label="Github Token"
+                    rules={[{ required: true }]}
+                    tooltip={
+                      <span>Github 的 <a href="" onClick={() => {craft.editorApi.openURL('https://github.com/settings/tokens/new');}}>Personal Token</a></span>
+                    }>
+                      <Input.Password placeholder={'Github Personal Token'} />
                   </Form.Item>
-                  <Form.Item name="url" label="url" rules={[{ required: true }]}>
-                    <Input placeholder={'其他'}  />
+                  <Form.Item name="github_owner" label="owner" rules={[{ required: true }]} tooltip={'Github 的用户名，如 "Xheldoon"'}>
+                    <Input placeholder={'用户名'}  />
+                  </Form.Item>
+                  <Form.Item name="github_repo" label="repo" rules={[{ required: true }]} tooltip={'Github 的仓库地址，只需要名字即可，如 "x_blog_src"'}>
+                    <Input placeholder={'仓库名'}  />
+                  </Form.Item>
+                  <Form.Item name="github_branch" label="branch" tooltip={'当前文档要上传到仓库的分支，默认是 "main"'}>
+                    <Input placeholder={'分支名'}  />
+                  </Form.Item>
+                  <Form.Item name="github_path" label="path" rules={[{ required: true }]} tooltip={'文件所在的路径，如 "_post/tech/readme.md" 本插件只支持文件'}>
+                    <Input placeholder={'文件路径'}  />
+                  </Form.Item>
+                  <Form.Item name="github_message" label="message" rules={[{ required: true }]} tooltip={'提交信息，如 "由 Craft github 插件添加"'}>
+                    <Input placeholder={'提交信息'}  />
                   </Form.Item>
                   <Form.Item {...tailLayout}>
-                    <Button type="primary" htmlType="submit" style={{margin: '0 8px'}}>
+                    <Button type="primary" htmlType="button" style={{margin: '0 8px'}} onClick={onFinish}>
                       确认并同步
                     </Button>
                     {
-                      Boolean(form.getFieldValue('config')) ? <Button htmlType="button" onClick={onDelete}>
+                      Boolean(getFieldValue('config')) ? <Button htmlType="button" onClick={onDelete}>
                         删除配置
                       </Button> : <Button htmlType="button" onClick={onReset}>
                         重置输入
